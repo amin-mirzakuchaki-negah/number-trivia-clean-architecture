675a75580e587a11d972b6eb3e1d9f0a512f374d
amin-mirzakuchaki-negah <amin.mirzakuchaki.negah@gmail.com>
2023-08-08 15:39:30 +0330

    Added required probs in implementaion of data repository

--- lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -1,6 +1,6 @@
 import '../models/number_trivia_model.dart';
 
-abstract class NumberTriviaRemoteDataSource {
+abstract class NumberTriviaLocalDataSource {
   Future<NumberTriviaModel> getLastNumberTrivia();
   Future<void> cacheLastNumberTrivia(NumberTriviaModel numberTriviaToCache);
 }
--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -3,7 +3,23 @@ import 'package:number_trivia_clean_architecture/core/error/failure.dart';
 import 'package:number_trivia_clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia_clean_architecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
+import '../../../../core/platform/network_info.dart';
+import '../datasources/number_trivia_local_data_source.dart';
+import '../datasources/number_trivia_remote_data_source.dart';
+
 class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+
+  final NumberTriviaRemoteDataSource remoteDataSource;
+  final NumberTriviaLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  NumberTriviaRepositoryImpl({
+    required this.remoteDataSource,
+    required this.localDataSource,
+    required this.networkInfo,
+  });
+
+
   @override
   Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) => throw UnimplementedError();
 
