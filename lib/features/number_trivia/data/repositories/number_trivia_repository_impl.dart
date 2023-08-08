14869e4319eaaffda924d33881eb1d3e61f36d44
amin-mirzakuchaki-negah <amin.mirzakuchaki.negah@gmail.com>
2023-08-08 16:42:55 +0330

    Added try catch to repo impl

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -22,16 +22,26 @@ class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
   @override
   Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(
       int number) async {
-    networkInfo.isConnected;
-    try {
-      final remoteTrivia =
-          await remoteDataSource.getConcreteNumberTrivia(number);
-      localDataSource.cacheLastNumberTrivia(remoteTrivia);
-      return Right(remoteTrivia);
-    } // 
-    // ignore: empty_catches
-    on ServerException {
-      return const Left(ServerFailure());
+    if (await networkInfo.isConnected) {
+      try {
+        final remoteTrivia =
+            await remoteDataSource.getConcreteNumberTrivia(number);
+        localDataSource.cacheLastNumberTrivia(remoteTrivia);
+        return Right(remoteTrivia);
+      } //
+      on ServerException {
+        return Left(ServerFailure());
+      }
+    } //
+    else {
+      try {
+        final localTrivia = await localDataSource.getLastNumberTrivia();
+        return Right(localTrivia);
+      }
+      //
+      on CacheException {
+        return Left(CacheFailure());
+      }
     }
   }
 
