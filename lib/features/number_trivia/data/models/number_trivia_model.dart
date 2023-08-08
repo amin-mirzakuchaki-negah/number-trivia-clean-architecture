6d9ff991f7a40ad10a142edd61fe7eb01444bf2c
amin-mirzakuchaki-negah <amin.mirzakuchaki.negah@gmail.com>
2023-08-08 15:11:01 +0330

    Refactored number trivia model codes

--- lib/features/number_trivia/data/models/number_trivia_model.dart
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -1,18 +1,9 @@
 import '../../domain/entities/number_trivia.dart';
 
 class NumberTriviaModel extends NumberTrivia {
-  // const NumberTriviaModel({required String text, required int number})
-  //     : super(text: text, number: number);
   const NumberTriviaModel({required super.number, required super.text}); //ask
 
-  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) { //Ask: why factory: i think because this function is returning an instance of NumberTriviaModel
-    return NumberTriviaModel(text: json['text'], number: json['number']);
-  }
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) => NumberTriviaModel(text: json['text'], number: json['number']);
 
-  Map<String, dynamic> toJson() {
-    return {
-      'text': text,
-      'number': number,
-    };
-  }
+  Map<String, dynamic> toJson() => {'text': text, 'number': number};
 }
