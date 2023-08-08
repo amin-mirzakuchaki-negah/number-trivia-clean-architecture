25264bb57305868a305f20e680add7d84bf7ae2f
amin-mirzakuchaki-negah <amin.mirzakuchaki.negah@gmail.com>
2023-08-08 12:51:50 +0330

    Added fromJson method

--- lib/features/number_trivia/data/models/number_trivia_model.dart
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -1,5 +1,12 @@
 import '../../domain/entities/number_trivia.dart';
 
 class NumberTriviaModel extends NumberTrivia {
-  const NumberTriviaModel({required super.text, required super.number});
+
+  // const NumberTriviaModel({required String text, required int number})
+  //     : super(text: text, number: number);
+  const NumberTriviaModel({required super.number, required super.text}); //ask
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(text: json['text'], number: json['number']);
+  }
 }
