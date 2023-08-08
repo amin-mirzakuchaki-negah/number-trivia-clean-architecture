8453bfc0d535da24ad687c82eda00e30f15825f1
amin-mirzakuchaki-negah <amin.mirzakuchaki.negah@gmail.com>
2023-08-08 14:14:48 +0330

    Created network information class

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,3 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:number_trivia_clean_architecture/core/error/failure.dart';
+import 'package:number_trivia_clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia_clean_architecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
-class NumberTriviaRepositoryImpl implements NumberTriviaRepository {}
+class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+  @override
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() {
+    throw UnimplementedError();
+  }
+}
