c302aafe0cf047e3d7632a2b8be3a0b52c61d900
amin-mirzakuchaki-negah <amin.mirzakuchaki.negah@gmail.com>
2023-08-09 12:30:43 +0330

    Removed not needed comments

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -4,7 +4,7 @@ abstract class Failure extends Equatable {
   const Failure();
 
   @override
-  List<Object?> get props => []; //why here we have to pass empty list but in number_trivia file we pass text and number
+  List<Object?> get props => [];
 }
 
 //General failures
