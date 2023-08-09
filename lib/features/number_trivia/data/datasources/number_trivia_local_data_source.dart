eb31b87c2b2aadae9e4c1cd5b10802e79ff60794
amin-mirzakuchaki-negah <amin.mirzakuchaki.negah@gmail.com>
2023-08-09 09:54:21 +0330

    Created implementaion of number trivia local data source

--- lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -1,6 +1,27 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 import '../models/number_trivia_model.dart';
 
 abstract class NumberTriviaLocalDataSource {
   Future<NumberTriviaModel> getLastNumberTrivia();
   Future<void> cacheLastNumberTrivia(NumberTriviaModel numberTriviaToCache);
+}
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+
+  final SharedPreferences sharedPreferences;
+
+  NumberTriviaLocalDataSourceImpl({required this.sharedPreferences});
+
+  @override
+  Future<void> cacheLastNumberTrivia(NumberTriviaModel numberTriviaToCache) {
+    // TODO: implement cacheLastNumberTrivia
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    // TODO: implement getLastNumberTrivia
+    throw UnimplementedError();
+  }
 }
