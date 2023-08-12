5119ad7f6add63053b6448b5b93b2578de7da3ef
amin-mirzakuchaki-negah <amin.mirzakuchaki.negah@gmail.com>
2023-08-12 15:09:34 +0330

    Added number trivia page

---
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class NumberTriviaPage extends StatelessWidget {
+  const NumberTriviaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Number Trivia'),
+      ),
+    );
+  }
+}
