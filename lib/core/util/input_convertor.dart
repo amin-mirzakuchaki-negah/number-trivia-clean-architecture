292b986679730109a1de0a26ee27e7c5887e9b39
amin-mirzakuchaki-negah <amin.mirzakuchaki.negah@gmail.com>
2023-08-12 10:12:41 +0330

    Created input convertor

---
+++ lib/core/util/input_convertor.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+
+import '../error/failure.dart';
+
+class InputConvertor {
+  Either<Failure, int> stringToUnsignedInt(String str) {
+    try {
+      final integer =int.parse(str);
+      if(integer < 0) throw const FormatException();
+      return Right(integer);
+    } //
+    on FormatException {
+      return Left(InvalidInputFailure());
+    }
+  } 
+}
+
+class InvalidInputFailure extends Failure {}
